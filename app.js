b6748d79181b29e669508b64ada520a9aa87445d
grigoriykuz18-netizen <grigoriykuz18@gmail.com>
2026-06-12 20:44:07 +0500

    Update app.js

--- app.js
+++ app.js
@@ -129,12 +129,10 @@ function renderSpots() {
 if (spot.claimed) {
   el.innerHTML = `
     <div class="claimed-avatar ${spot.tier}">
-      <div class="avatar">
-        ${initials(spot.name || 'A')}
-      </div>
+      <div class="avatar-placeholder"></div>
     </div>
   `;
-}
+}                     
     el.addEventListener('mouseenter', e => showTooltip(e, spot));
     el.addEventListener('mousemove', e => moveTooltip(e));
     el.addEventListener('mouseleave', hideTooltip);
